4aef922ba152d52ef0eb70051e7f0b4e662d6a27
iwana994994 <iwana994994@gmail.com>
2025-10-02 21:23:11 +0200

    fixing index on backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -51,23 +51,16 @@ app.use("/api/songs",songsRouter)
 app.use("/api/albums",albumsRouter)
 
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname, "..", "frontend", "build")))
-  app.get("*", (req, res) => {
-     console.log("Catch-all route triggered for URL:", req.url);
-    res.sendFile(path.join(__dirname, "../frontend/build/index.html"))
-  })
+  app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+  });
 }
 
-app.use((req, res, next) => {
-  console.log(`Request method: ${req.method}, url: ${req.url}`);
-  next();
-});
-
-
 httpServer.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-    connectDB()
-})
+    console.log("Server is running on port " + PORT);
+    connectDB();
+});
 
