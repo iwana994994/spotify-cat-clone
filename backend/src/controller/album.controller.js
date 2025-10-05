f930032c8e84fdd4f1f305d0a6443c636d615d85
iwana994994 <iwana994994@gmail.com>
2025-10-05 23:56:49 +0200

    adding albums on backend and frontend

--- backend/src/controller/album.controller.js
+++ backend/src/controller/album.controller.js
@@ -1,2 +1,27 @@
-export const getAllAlbums=async(req,res,next)=>{}
-export const getAlbumById=async(req,res,next)=>{}
+import Album from "../model/album.model.js";
+export const getAllAlbums=async(req,res,next)=>{
+    try {
+        const albums = await Album.find(); // Assuming you have an Album model
+        res.status(200).json(albums);
+    }
+    catch (error) {
+        console.error("Error fetching albums:", error);
+        res.status(500).json({ message: "Error fetching albums" });
+    }
+}
+export const getAlbumById=async(req,res,next)=>{
+    try{
+        const {id}= req.params;
+        const album= await Album.findById(id).populate("songs")// Assuming you have an Album model
+    
+        if (!album) {
+            return res.status(404).json({ message: "Album not found" });
+        }  
+        res.status(200).json(album);
+    }
+    
+    catch (error) {
+        console.error("Error fetching album:", error);
+        res.status(500).json({ message: "Error fetching album" }); 
+    }
+}
