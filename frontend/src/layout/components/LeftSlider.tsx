df68f07551921fc18b8616f0756e1ccb85cb77f8
iwana994994 <iwana994994@gmail.com>
2025-10-05 23:13:21 +0200

    adding LeftSideBar

--- frontend/src/layout/components/LeftSlider.tsx
+++ frontend/src/layout/components/LeftSlider.tsx
@@ -1,23 +1,73 @@
-import { Home, MessageCircle } from "lucide-react"
-import { Link } from "react-router-dom"
+import PlayListsSkeleton from "../../skeletons/PlayListsSkeleton.tsx"
+import { ScrollArea } from '@/components/ui/scroll-area'
+import { useMusicStore } from '@/stores/useMusicStore.tsx'
+import { SignedIn} from '@clerk/clerk-react'
+import { HomeIcon, Library, MessageCircle} from 'lucide-react'
+import { useEffect } from 'react'
+ import { Link } from 'react-router-dom'
 
 
-const LeftSlider = () => {
+const LeftSideBar = () => {
+ const {albums,isLoaded,fetchAlbums} = useMusicStore()
+
+
+ useEffect(() => {
+ fetchAlbums()
+ }, [fetchAlbums])
+
+ console.log({albums})
+
+
+
   return (
-    <div className=" space-y-2 m-2">
-        <Link to="/" className="flex items-center gap-2  hover:bg-zinc-700 rounded-lg p-2">
-        <Home className="h-6 w-6" />
-        <span>Home</span>
-        </Link>
-           <Link to="/chat" className="flex items-center gap-2  hover:bg-zinc-700 rounded-lg p-2">
-        <MessageCircle className="h-6 w-6" />
-        <span>Chat</span>
-        </Link>
-        
+    <div className='p-2'>
+    {/* Navigation links for the left sidebar */}  
+   <div className="rounded-lg p-2 hover:bg-zinc-500">
+  <Link to="/" className="flex items-center gap-2 text-white">
+    <HomeIcon className="size-6" />
+    <span className="hidden md:inline">Home</span>
+  </Link>
+</div>
 
+<div className="rounded-lg p-2 hover:bg-zinc-500">
+  <SignedIn>
+    <Link to="/chat" className="flex items-center gap-2 text-white">
+      <MessageCircle className="size-6" />
+      <span className="hidden md:inline">Message</span>
+    </Link>
+  </SignedIn>
+</div>
+
+   {/*Library link for albums*/} 
+    <div className='rounded-lg p-2 bg-gray-500'>
+      <div>
+     {/*Name of the library*/}
+            <div className='flex items-center justify-between'>
+        <Library className='size-6 mt-2' />
+        <span className='text-white text-sm font-semibold hidden md:inline'>Library</span>
+      </div>
+      </div>
+
+<ScrollArea className="mt-2 h-[calc(100vh-300px)]">
+  <div className='space-y-2'>
+    {isLoaded ? (
+      <PlayListsSkeleton />
+    ) : (
+      albums.map((album) => (
+        <Link to={`/albums/${album._id}`} key={album._id} className=' hover:bg-gray-400 flex items-center justify-between p-4 rounded-lg mb-2 bg-zinc-600'>
+          
+            <img src={album.imageUrl} alt={album.title} className='h-10 w-10 rounded-full' />
+            <p>{album.title}</p>
+        </Link>
+      ))
+    )}
+  </div>
+</ScrollArea>
+    </div>
+    
 
     </div>
   )
 }
 
-export default LeftSlider
+export default LeftSideBar
